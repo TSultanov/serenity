c5a1fb0304ef65cff428ebceaf63e8060e158ddf
Timur Sultanov <tssultanov@outlook.com>
2022-06-12 13:53:49 +0300

    Ports: X util-macros port

---
+++ Ports/util-macros/package.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env -S bash ../.port_include.sh
+port=util-macros
+version=1.19.3
+useconfigure="true"
+use_fresh_config_sub="false"
+files="https://www.x.org/releases/individual/util/util-macros-${version}.tar.gz util-macros-${version}.tar.gz"
