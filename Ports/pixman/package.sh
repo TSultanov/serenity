a35582e00ea296fabc0faca6eca2daf005872c72
Timur Sultanov <tssultanov@outlook.com>
2022-06-14 02:13:43 +0300

    Ports: Convert pixman to use meson

--- Ports/pixman/package.sh
+++ Ports/pixman/package.sh
@@ -4,4 +4,17 @@ version='0.40.0'
 useconfigure='true'
 files="https://www.cairographics.org/releases/pixman-${version}.tar.gz pixman-${version}.tar.gz 6d200dec3740d9ec4ec8d1180e25779c00bc749f94278c8b9021f5534db223fc"
 auth_type='sha256'
-use_fresh_config_sub='true'
+configopts=("--cross-file" "${SERENITY_BUILD_DIR}/meson-cross-file.txt")
+
+configure() {
+    run meson _build "${configopts[@]}"
+}
+
+build() {
+    run ninja -C _build
+}
+
+install() {
+    export DESTDIR="${SERENITY_INSTALL_ROOT}"
+    run meson install -C _build
+}
