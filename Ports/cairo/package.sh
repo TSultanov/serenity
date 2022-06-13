dea6a77a3cd90505ed9fc2bf46df44b31983315b
Timur Sultanov <tssultanov@outlook.com>
2022-06-14 02:18:58 +0300

    Ports: Convert cairo to use meson

--- Ports/cairo/package.sh
+++ Ports/cairo/package.sh
@@ -5,5 +5,23 @@ useconfigure="true"
 config_sub_paths=("build/config.sub")
 use_fresh_config_sub="true"
 files="https://cairographics.org/snapshots/cairo-${version}.tar.xz cairo-${version}.tar.xz"
-configopts=("--with-sysroot=${SERENITY_INSTALL_ROOT}")
-depends=("libpng" "pixman" "fontconfig")
+configopts=("--cross-file" "cross_file.txt")
+depends=("libpng" "pixman" "fontconfig" "glib" "libexpat")
+
+pre_configure() {
+    run cp "../cross_file-${SERENITY_ARCH}${SERENITY_TOOLCHAIN}.txt" "cross_file.txt"
+    run sed "s%SERENITY_BUILD_DIR%${SERENITY_BUILD_DIR}%g" -i "cross_file.txt"
+}
+
+configure() {
+    run meson _build "${configopts[@]}"
+}
+
+build() {
+    run ninja -C _build
+}
+
+install() {
+    export DESTDIR="${SERENITY_INSTALL_ROOT}"
+    run meson install -C _build
+}
