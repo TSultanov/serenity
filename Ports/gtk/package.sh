a8848d9d8c47eb3328c6f4e1994f85c06e44730d
Timur Sultanov <tssultanov@outlook.com>
2022-06-15 00:35:36 +0300

    Ports: gtk updates

--- Ports/gtk/package.sh
+++ Ports/gtk/package.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env -S bash ../.port_include.sh
-port=gtk
+port=gtk+
 version=3.24.34
 useconfigure="true"
-config_sub_paths=("config.sub")
+config_sub_paths=("build-aux/config.sub")
 use_fresh_config_sub="true"
 files="https://download.gnome.org/sources/gtk+/3.24/gtk%2B-${version}.tar.xz gtk+-${version}.tar.xz"
 configopts=("")
