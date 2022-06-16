93183eb2067010981618a5ee29d5749b1fb6e471
Timur Sultanov <tssultanov@outlook.com>
2022-06-17 01:35:46 +0300

    Ports: Add older cairo port which doesn't require xrender

--- Ports/cairo/package.sh
+++ Ports/cairo/package.sh
@@ -1,27 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=cairo
-version=1.17.4
+version=1.11.4
 useconfigure="true"
 config_sub_paths=("build/config.sub")
 use_fresh_config_sub="true"
-files="https://cairographics.org/snapshots/cairo-${version}.tar.xz cairo-${version}.tar.xz"
-configopts=("--cross-file" "cross_file.txt" "-Dxlib=enabled")
+files="https://cairographics.org/snapshots/cairo-${version}.tar.gz cairo-${version}.tar.gz"
+configopts=("--with-sysroot=${SERENITY_INSTALL_ROOT}")
 depends=("libpng" "pixman" "fontconfig" "glib" "libexpat")
-
-pre_configure() {
-    run cp "../cross_file-${SERENITY_ARCH}${SERENITY_TOOLCHAIN}.txt" "cross_file.txt"
-    run sed "s%SERENITY_BUILD_DIR%${SERENITY_BUILD_DIR}%g" -i "cross_file.txt"
-}
-
-configure() {
-    run meson _build "${configopts[@]}"
-}
-
-build() {
-    run ninja -C _build
-}
-
-install() {
-    export DESTDIR="${SERENITY_INSTALL_ROOT}"
-    run meson install -C _build
-}
