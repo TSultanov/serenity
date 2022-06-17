883c4e5fb2c5d7238b9a652d222bb325fcc4ec0b
Timur Sultanov <tssultanov@outlook.com>
2022-06-17 21:38:08 +0300

    LibXEmulatorExt: Fix stubs.cpp with GCC

--- Userland/Libraries/LibXEmulatorExt/stubs.cpp
+++ Userland/Libraries/LibXEmulatorExt/stubs.cpp
@@ -1,7 +1,9 @@
 #include <X11/Xlib.h>
-//#include <X11/extensions/extutil.h>
+//#include <X11/extensions/xex>
 
 extern "C" {
+
+void* XextFindDisplay(void* /*extinfo*/, Display* /*dpy*/);
 
 void* XextFindDisplay(void* /*extinfo*/, Display* /*dpy*/)
 {
