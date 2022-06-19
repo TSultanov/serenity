db9382f57fb471f2e0f025071c0fd72e9eb089d8
Timur Sultanov <tssultanov@outlook.com>
2022-06-19 13:48:35 +0300

    LibXEmulator: Add XFreePixmap function

--- Userland/Libraries/LibXEmulator/Pixmap.cpp
+++ Userland/Libraries/LibXEmulator/Pixmap.cpp
@@ -1,4 +1,5 @@
 #include "XPixmap.h"
+#include "ObjectManager.h"
 
 namespace XLib {
 extern "C" {
@@ -13,4 +14,14 @@ XLib::XCreatePixmap(Display* display, Drawable /*d*/,
     Gfx::IntRect rect(0, 0, width, height);
     auto pixmap = MUST(XPixmap::try_create(display, rect));
     return pixmap->id();
+}
+
+extern "C" int
+XLib::XFreePixmap(Display */*display*/, Pixmap pxm)
+{
+    auto pixmap = ObjectManager::the().get_pixmap(pxm);
+    if (pixmap.is_null())
+        return BadPixmap;
+    ObjectManager::the().remove_pixmap(pxm);
+    return Success;
 }
