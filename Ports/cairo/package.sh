df88b9e7aef2ec0c97d98f12585959b7dc6f1d07
Timur Sultanov <tssultanov@outlook.com>
2022-06-19 16:35:59 +0300

    Ports: Use cairo 1.12.2 as newest not requiring shm

--- Ports/cairo/package.sh
+++ Ports/cairo/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=cairo
-version=1.11.4
+version=1.12.2
 useconfigure="true"
 config_sub_paths=("build/config.sub")
 use_fresh_config_sub="true"
-files="https://cairographics.org/snapshots/cairo-${version}.tar.gz cairo-${version}.tar.gz"
+files="https://www.cairographics.org/releases/cairo-${version}.tar.xz cairo-${version}.tar.xz"
 configopts=("--with-sysroot=${SERENITY_INSTALL_ROOT}" "--enable-xlib")
 depends=("libpng" "pixman" "fontconfig" "glib" "libexpat")
