b753c2f73f275acbc408fbfafa450fb0ff1646c1
Timur Sultanov <tssultanov@outlook.com>
2022-06-20 01:11:22 +0300

    Ports: Add haiku patches to gtk port

--- Ports/gtk/package.sh
+++ Ports/gtk/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=gtk+
-version=3.24.34
+version=3.24.30
 useconfigure="true"
 config_sub_paths=("build-aux/config.sub")
 use_fresh_config_sub="true"
 files="https://download.gnome.org/sources/gtk+/3.24/gtk%2B-${version}.tar.xz gtk+-${version}.tar.xz"
-configopts=("")
-depends=("cairo" "fontconfig")
+configopts=("--with-sysroot=${SERENITY_INSTALL_ROOT}")
+depends=("cairo" "fontconfig" "atk" "pango" "gdk-pixbuf")
---
+++ Ports/gtk/patches/ReadMe.md
@@ -0,0 +1,7 @@
+# Patches for gtk+ on SerenityOS
+
+## `0001-Apply-Haiku-patches-with-some-changes.patch`
+
+Apply Haiku patches with some changes
+
+
