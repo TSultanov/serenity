d97a717577790495683071d8f0b3a5e75b393eb2
Timur Sultanov <tssultanov@outlook.com>
2022-06-19 14:12:01 +0300

    LibXEmulator: Add XInitImage function

--- Userland/Libraries/LibXEmulator/Image.cpp
+++ Userland/Libraries/LibXEmulator/Image.cpp
@@ -8,6 +8,108 @@ extern "C" {
 }
 }
 
+static int
+DestroyImage(XLib::XImage* image)
+{
+    free(image->data);
+    delete image;
+    return Success;
+}
+
+static inline uint8_t*
+GetImageDataPointer(XLib::XImage* image, int x, int y)
+{
+    return (uint8_t*)&(image->data[
+        (y * image->bytes_per_line) + ((x * image->bitmap_unit) / CHAR_BIT)]);
+}
+
+static unsigned long
+ImageGetPixel(XLib::XImage* image, int x, int y)
+{
+    unsigned long pixel = 0;
+    uint8_t* srcPtr = GetImageDataPointer(image, x, y);
+
+    switch (image->bits_per_pixel) {
+    case 1:
+        pixel = ((*srcPtr) & (0x80 >> (x % 8))) ? 1 : 0;
+        break;
+    case 8:
+        pixel = *srcPtr;
+        break;
+    case 15:
+    case 16:
+        pixel = srcPtr[0] | (srcPtr[1] << 8);
+        break;
+    case 24:
+        pixel = srcPtr[0] | (srcPtr[1] << 8) | (srcPtr[2] << 16);
+        break;
+    case 32:
+        pixel = srcPtr[0] | (srcPtr[1] << 8) | (srcPtr[2] << 16) | (srcPtr[3] << 24);
+        break;
+    }
+    return pixel;
+}
+
+static int
+ImagePutPixel(XLib::XImage* image, int x, int y, unsigned long pixel)
+{
+    uint8_t* destPtr = GetImageDataPointer(image, x, y);
+    uint8_t* srcPtr = (uint8_t*)&pixel;
+    switch (image->bits_per_pixel) {
+    case 1: {
+        int mask = (0x80 >> (x % 8));
+        if (pixel) {
+            (*destPtr) |= mask;
+        } else {
+            (*destPtr) &= ~mask;
+        }
+        break;
+    }
+    case 8:
+        *destPtr = pixel;
+        break;
+    case 15:
+    case 16:
+        destPtr[0] = srcPtr[0];
+        destPtr[1] = srcPtr[1];
+        break;
+    case 24: {
+        destPtr[0] = srcPtr[0];
+        destPtr[1] = srcPtr[1];
+        destPtr[2] = srcPtr[2];
+        break;
+    }
+    case 32:
+        destPtr[0] = srcPtr[0];
+        destPtr[1] = srcPtr[1];
+        destPtr[2] = srcPtr[2];
+        destPtr[3] = srcPtr[3];
+        break;
+    }
+    return 0;
+}
+
+
+extern "C" Status
+XLib::XInitImage(XImage* image)
+{
+    if (image->bytes_per_line == 0) {
+        image->bytes_per_line = image->width * (image->bitmap_unit / 8);
+
+		const int align = image->bitmap_pad / 8;
+        if (align)
+            image->bytes_per_line = ((image->bytes_per_line + align - 1) / align) * align;
+    }
+
+    memset(&image->f, 0, sizeof(image->f));
+    image->f.destroy_image = DestroyImage;
+    image->f.get_pixel = ImageGetPixel;
+    image->f.put_pixel = ImagePutPixel;
+
+    return 1;
+}
+
+
 extern "C" XLib::XImage*
 XLib::XGetSubImage(Display* /*display*/, Drawable d,
     int x, int y, unsigned int width, unsigned int height,
