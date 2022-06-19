cad7c52a77f90bce4f3871049ed5d966c95686ef
Timur Sultanov <tssultanov@outlook.com>
2022-06-19 23:29:48 +0300

    Ports: Remove pixman dep from cairo

--- Ports/cairo/package.sh
+++ Ports/cairo/package.sh
@@ -6,4 +6,4 @@ config_sub_paths=("build/config.sub")
 use_fresh_config_sub="true"
 files="https://cairographics.org/snapshots/cairo-1.17.4.tar.xz cairo-${version}.tar.xz"
 configopts=("--with-sysroot=${SERENITY_INSTALL_ROOT}" "--enable-xlib")
-depends=("libpng" "pixman" "fontconfig" "glib" "libexpat")
+depends=("libpng" "fontconfig" "glib" "libexpat")
