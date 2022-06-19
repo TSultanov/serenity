1a03dede341ae9e559ae50f3e3a118ef7b21fdbe
Timur Sultanov <tssultanov@outlook.com>
2022-06-19 23:18:26 +0300

    Ports: Make cairo 1.17.4 compilable

--- Ports/cairo/package.sh
+++ Ports/cairo/package.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env -S bash ../.port_include.sh
 port=cairo
-version=1.12.2
+version=1.17.4
 useconfigure="true"
 config_sub_paths=("build/config.sub")
 use_fresh_config_sub="true"
-files="https://www.cairographics.org/releases/cairo-${version}.tar.xz cairo-${version}.tar.xz"
+files="https://cairographics.org/snapshots/cairo-1.17.4.tar.xz cairo-${version}.tar.xz"
 configopts=("--with-sysroot=${SERENITY_INSTALL_ROOT}" "--enable-xlib")
 depends=("libpng" "pixman" "fontconfig" "glib" "libexpat")
---
+++ Ports/cairo/patches/ReadMe.md
@@ -0,0 +1,12 @@
+# Patches for cairo on SerenityOS
+
+## `0001-Remove-missing-headers.patch`
+
+Remove missing headers
+
+
+## `0002-Remove-Shm-check.patch`
+
+Remove Shm check
+
+
