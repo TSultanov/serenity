fd0a0e64cc8d8fbb55ceebf777025fe538d1dea4
Timur Sultanov <tssultanov@outlook.com>
2022-06-19 14:34:56 +0300

    LibXEmulator: Add XQueryColors

--- Userland/Libraries/LibXEmulator/Color.cpp
+++ Userland/Libraries/LibXEmulator/Color.cpp
@@ -23,3 +23,18 @@ XLib::XAllocColor(Display* /*dpy*/, Colormap /*cmap*/, XColor* def)
     def->pixel = _x_rgb_to_pixel(Gfx::Color(def->red, def->green, def->blue));
     return 1;
 }
+
+extern "C" Status
+XLib::XQueryColors(Display */*display*/, Colormap /*colormap*/,
+    XColor *defs_in_out, int ncolors)
+{
+    for (int i = 0; i < ncolors; i++) {
+        Gfx::Color color = _x_pixel_to_rgb(defs_in_out[i].pixel);
+        defs_in_out[i].red = color.red();
+        defs_in_out[i].green = color.green();
+        defs_in_out[i].blue = color.blue();
+        defs_in_out[i].flags = DoRed | DoGreen | DoBlue;
+        defs_in_out[i].pad = 0;
+    }
+    return Success;
+}
--- Userland/Libraries/LibXEmulator/Color.h
+++ Userland/Libraries/LibXEmulator/Color.h
@@ -23,3 +23,15 @@ _x_rgb_to_pixel(Color color)
         | (long(color.red()) << 16) | (long(color.alpha()) << 24);
     return result;
 }
+
+static inline Gfx::Color
+_x_pixel_to_rgb(unsigned long color, bool alpha = false)
+{
+    Gfx::Color rgb;
+    rgb.set_red((color >> 16) & 0xFF);
+    rgb.set_green((color >> 8) & 0xFF);
+    rgb.set_blue(color & 0xFF);
+    if (alpha)
+        rgb.set_alpha((color >> 24) & 0xFF);
+    return rgb;
+}
