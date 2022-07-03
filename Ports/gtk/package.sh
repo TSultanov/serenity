1eedc6fbf46fb52bc44bcac1328fb31a9dbfda60
Timur Sultanov <tssultanov@outlook.com>
2022-07-04 00:12:50 +0300

    Ports: gtk

--- Ports/gtk/package.sh
+++ Ports/gtk/package.sh
@@ -6,6 +6,6 @@ config_sub_paths=("build-aux/config.sub")
 use_fresh_config_sub="true"
 files="https://download.gnome.org/sources/gtk+/3.24/gtk%2B-${version}.tar.xz gtk+-${version}.tar.xz"
 configopts=("--with-sysroot=${SERENITY_INSTALL_ROOT}")
-depends=("cairo" "fontconfig" "atk" "pango" "gdk-pixbuf")
+depends=("cairo" "fontconfig" "atk" "pango" "gdk-pixbuf" "libepoxy" "fribidi")
 
 export LDFLAGS="-lcore -lgui -lgfx"
